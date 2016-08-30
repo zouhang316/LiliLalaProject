d089018fe66f394340164a2475b01da40405dd7c
zh_android <497239511@qq.com>
2016-08-30 11:37:12 +0800

    增加ArticleActivity

---
+++ app/src/main/java/com/android/lala/activity/ArticleActivity.java
@@ -0,0 +1,31 @@
+package com.android.lala.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.android.lala.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2016/8/30.
+ */
+public class ArticleActivity extends BaseActivity implements View.OnClickListener{
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected boolean isShowToolBar() {
+        return false;
+    }
+
+    @Override
+    protected void onActivityCreate(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
